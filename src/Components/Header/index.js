5c9fd0d277e985d908bfd7bcc2cfbf3aaf0e747f
yosoares <laurinhassoares04@gmail.com>
2023-12-04 16:09:21 -0300

    cep

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,25 +1,29 @@
-<<<<<<< HEAD
+// <<<<<<< HEAD
 import { Link } from 'react-router-dom';
 import './estilo.css';
 import icon from './icon.png';
 
 export default function Header() {
-    return (
-        <div className='containersp'>
-            <div className='icon'>
-                <Link to='/'>
-                    <a href='icon'><img alt='icon' src={icon}></img></a>
-                </Link>
-                <div className='css'>
-                    <a>Cadastro</a>
-                    <div className='dropdown-content'>
-                        <Link to='./Vendas'>Cliente</Link>
-                        <Link to='./PessoaF'>Pessoa Física</Link>
-                        <Link to='./'>Pessoa Jurídica</Link>
-                        <Link to='./Manipulacao'> Dados </Link>
-                    </div>
-=======
-import { Link } from "react-router-dom";
+  return (
+    <div className='containersp'>
+      <div className='icon'>
+        <Link to='/'>
+          <a href='icon'><img alt='icon' src={icon}></img></a>
+        </Link>
+        <div className='css'>
+          <a>Cadastro</a>
+          <div className='dropdown-content'>
+            <Link to='./Vendas'>Cliente</Link>
+            <Link to='./PessoaF'>Pessoa Física</Link>
+            <Link to='./'>Pessoa Jurídica</Link>
+            <Link to='./Manipulacao'> Dados </Link>
+          </div>
+        </div>
+      </div>
+    </div>)
+}
+// =======
+/* import { Link } from "react-router-dom";
 import "./estilo.css";
 import icon from "./icon.png";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -65,6 +69,4 @@ export default function Header() {
           </div>
         </div>
       </div>
-    </div>
-  );
-}
+    </div> */
